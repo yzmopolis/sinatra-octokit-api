2e9fae1c99058ae8339b8310338e4a0085a6a864
ika.grabon <ika.grabon@gmail.com>
2018-01-04 21:36:49 +0100

    Add active record

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'octokit'
 require 'sinatra'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+    :adapter => 'sqlite3',
+    :database =>  'sinatra_application.sqlite3.db'
+)
 
 module GithubController
 
